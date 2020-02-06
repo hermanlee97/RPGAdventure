4d18d0bd9f825fe7ba66a11c4ec89faf6afd285b
hermanlee97 <leeh91@uni.coventry.ac.uk>
2020-02-06 17:32:10 +0000

    basic structure for events.cpp

---
+++ events.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int options() {
+    // options to move, fight, flight
+}
+
+int move() {
+    // options for the directions to move 
+}
--- main.cpp
+++ main.cpp
@@ -2,6 +2,5 @@
 using namespace std;
 
 int main() {
-    //test map by making a simple 2x2
     return 0;
 }
