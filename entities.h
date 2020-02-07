bd87d5248b10629e850246452f6f1147da86c5fd
hanni02678 <you.play.we.record@gmail.com>
2020-02-07 13:32:08 +0000

    Entities with combat method on player.

---
+++ entities.h
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <windows.h>
+
+class Entity
+{
+public:
+    int hp;
+    int damage;
+    int defence;
+    int gold;
+};
+
+class Player : public Entity
+{
+public:
+    int combat(Entity player, Entity enemy)
+    {
+        int pHP = player.hp;
+        int nHP = enemy.hp;
+
+        while (nHP > 0 && pHP > 0)
+        {
+
+            // DEAL DAMAGE TO ENEMY
+            if ((enemy.defence - player.damage) < 0)
+            {
+                nHP += enemy.defence - player.damage;
+            }
+            else
+            {
+                nHP -= 0;
+            }
+
+            // CHECK IF THE ENEMY IS DEAD
+            if (nHP <= 0)
+            {
+                return pHP;
+            }
+            else
+            {
+                // DEAL DAMAGE TO PLAYER
+                if ((player.defence - enemy.damage) < 0)
+                {
+                    pHP += player.defence - enemy.damage;
+                }
+                else
+                {
+                    pHP -= 0;
+                }
+            }
+
+            // CHECK IF THE PLAYER IS DEAD
+            if (pHP <= 0)
+            {
+                // Death function, END GAME LOOP
+                return 0;
+            }
+        }
+
+        return pHP;
+    }
+};
+
+class Enemy : public Entity
+{
+};
