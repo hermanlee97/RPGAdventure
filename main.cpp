594591cc893bca746dedefc17cb304b8bb62cec0
hanni02678 <you.play.we.record@gmail.com>
2020-02-07 11:30:56 +0000

    The Basic Game Layout

--- main.cpp
+++ main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <string>
+#include <windows.h>
 using namespace std;
 
+// ## Personal header(.h) files ##
+// {replace}
+// {replace}
+
+//  ##################    MAIN FUNCTION       ###############
 int main()
 {
-    cout << "Hi !" << endl;
-    return 0;
-    cout << "Hey pepe!";
+    // ON START (Load/NewGame functions)
+    // {replace}
+    system("CLS");
+
+    // Starting position
+    int xPos = 3;
+    int yPos = 3;
+
+    // ON UPDATE
+    while (true)
+    {
+
+        // ## 1. World Change.  (New area/block loaded. Display the stuff on the screen.) ##
+        cout << "       Meadow" << endl;
+        cout << "    X: " << xPos << " Y: " << yPos << endl;
+
+        // ## 2. Event Presented. (Fight/Loot functions) ##
+        // {replace}
+
+        // ## 3. Futher Movement on the Map. (Move function (if the player did not die..)) ##
+        // {replace}
+
+        // ## 4. Wait. (For now) ##
+        getchar();     // Waits till you press a button.
+        system("CLS"); // Clears the Window.
+    }
+
+    return (0);
 }
