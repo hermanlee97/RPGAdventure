19368850018bcf4449f7a81651fc43f8a991ad75
hermanlee97 <leeh91@uni.coventry.ac.uk>
2020-02-10 20:21:33 +0000

    implemented options

--- events.cpp
+++ events.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
+#include <string>
+#include "events.h"
 using namespace std;
 
-int options() {
-    // options to move, fight, flight
-}
+void check_combat(map, x, y) {
+    if (map.content[x][y].get_has_enemy()){
+        cout << "Enemy appears!" << endl;
+        cout << "1. fight       2. flight" << endl;
+    }
 
-int move() {
-    // options for the directions to move 
+}
+void options(map, x, y) {
+    Block cur = map.content[x][y];
+    Block left = map.content[x-1][y];
+    Block right = map.content[x+1][y];
+    Block up = map.content[x][y-1];
+    Block down = map.content[x][y+1];
+
+    string ops;
+    int count = 1;
+    if (left.get_walkable()){
+        ops.append(to_string(count) + ". go left        ");
+        count += 1;
+    }
+    if (right.get_walkable()){
+        ops.append(to_string(count) + ". go right        ");
+        count += 1;
+    }
+    if (up.get_walkable()){
+        ops.append(to_string(count) + ". go up        ");
+        count += 1;
+    }
+    if (down.get_walkable()){
+        ops.append(to_string(count) + ". go down        ");
+        count += 1;
+    }
+    if (cur.get_has_npc()){
+        ops.append(to_string(count) + ". talk to npc        ");
+        count += 1;
+    }
+
+    cout << ops << endl;
 }
---
+++ events.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include "map.h"
+using namespace std;
+
+void check_combat(map, x, y);
+
+void options(map, x, y);
